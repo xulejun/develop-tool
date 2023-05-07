e79a6243f294e836ad6a8382a8e8ccc9a2a84b3b
LegendXu <xu-lejun@qq.com>
2023-05-07 18:14:30 +0800

    feat：jedis 连接优化

--- redis/src/main/java/com/legend/redis/jedis/JedisConnectDemo.java
+++ redis/src/main/java/com/legend/redis/jedis/JedisConnectDemo.java
@@ -1,11 +1,14 @@
 package com.legend.redis.jedis;
 
+import cn.hutool.core.util.StrUtil;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Value;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.JedisSentinelPool;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
@@ -18,17 +21,54 @@ import java.util.Set;
  */
 public class JedisConnectDemo {
 
+    private static final int DEFAULT_TIMEOUT = 15000;
+
+    /**
+     * 密码不能为空串，否则也报错
+     */
+    private static final String PASSWORD = null;
+
     public static void main(String[] args) throws Exception {
-        Jedis jedis = new Jedis("127.0.0.1", 6379);
-        jedis.auth("123456");
-        System.out.println(jedis.get("hello"));
+        // jedis 单节点
+        Jedis jedis = getSingleJedis();
+
+        jedis.close();
+
+        // jedis 连接池
+        JedisPool jedisPool = getJedisPool(null);
+        Jedis jedisPoolResource = jedisPool.getResource();
+
+        jedisPoolResource.close();
+
+        // jedis 哨兵模式
+        JedisSentinelPool sentinelPool = getJedisSentinelPool();
+        Jedis sentinelPoolResource = sentinelPool.getResource();
+
+        sentinelPoolResource.close();
     }
 
-    @Test
-    public void testSentinel() {
-        String masterName = "mymaster";
-        String password = "123456";
-        // 设置参数
+    /**
+     * jedis 单机连接
+     *
+     * @return
+     */
+    public static Jedis getSingleJedis() {
+        Jedis jedis = new Jedis("127.0.0.1", 6379);
+        if (StrUtil.isNotBlank(PASSWORD)) {
+            jedis.auth(PASSWORD);
+        }
+        return jedis;
+    }
+
+    /**
+     * jedis 连接池（可连接多个节点）
+     *
+     * @param address
+     * @return
+     */
+    public static JedisPool getJedisPool(String address) {
+        JedisPool jedisPool = null;
+        // 配置参数
         JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
         // 最大连接数, 默认8个
         jedisPoolConfig.setMaxTotal(10);
@@ -36,68 +76,47 @@ public class JedisConnectDemo {
         jedisPoolConfig.setMaxIdle(5);
         // 最小空闲连接数, 默认0
         jedisPoolConfig.setMinIdle(5);
+        // 最大等待时间
+        jedisPoolConfig.setMaxWait(Duration.ofMillis(30000));
+
+        String[] split = address.split(":");
+        if (StrUtil.isNotBlank(address)) {
+            String ip = split[0];
+            int port = Integer.parseInt(split[1]);
+            String password = split.length > 2 ? split[2] : null;
+            jedisPool = new JedisPool(jedisPoolConfig, ip, port, DEFAULT_TIMEOUT, password);
+        } else {
+            jedisPool = new JedisPool(jedisPoolConfig, "localhost", 6379, DEFAULT_TIMEOUT, PASSWORD);
+        }
+        return jedisPool;
+    }
+
+    /**
+     * jedis 哨兵模式（主从模式加强版），需要先搭建 redis 集群
+     *
+     * @return
+     */
+    public static JedisSentinelPool getJedisSentinelPool() {
+        String masterName = "myMaster";
+        String password = "123456";
+
+        // 配置参数
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        // 最大连接数, 默认8个
+        jedisPoolConfig.setMaxTotal(10);
+        // 最大空闲连接数, 默认8个
+        jedisPoolConfig.setMaxIdle(5);
+        // 最小空闲连接数, 默认0
+        jedisPoolConfig.setMinIdle(5);
+        // 最大等待时间
+        jedisPoolConfig.setMaxWait(Duration.ofMillis(30000));
+
         // 哨兵信息，注意填写哨兵的地址
         Set<String> sentinels = new HashSet<>();
         sentinels.add("127.0.0.1:6390");
         sentinels.add("127.0.0.1:6391");
         // 创建连接池，当master宕机后，哨兵模式自动切换到slave上
-//        JedisSentinelPool pool = new JedisSentinelPool(masterName, sentinels, jedisPoolConfig, password);
-//        if ("6381".equals(String.valueOf(pool.getCurrentHostMaster().getPort()))) {
-//            pool = new JedisSentinelPool(masterName, sentinels, jedisPoolConfig);
-//        }
-        JedisPool pool = new JedisPool("10.128.188.44", 6379);
-        // 获取客户端
-        try (Jedis jedis = pool.getResource()) {
-            jedis.set("username", "legendxu");
-            String value = jedis.get("username");
-            System.out.println(value);
-        } catch (Exception e) {
-            System.out.println("jedis异常：" + e);
-        }
-    }
-
-    @Test
-    public void testPassword() {
-        int DEFAULT_TIMEOUT = 120000;
-//        String addresses = "10.128.188.44:6379";
-        String addresses = "127.0.0.1:6379";
-        JedisPool[] pools;
-        String[] addrs;
-//        String password = "Octopus123@";
-        String password = "123456";
-
-        JedisPoolConfig config = new JedisPoolConfig();
-        config.setTestOnBorrow(true);
-        config.setMaxTotal(200);
-        config.setMaxIdle(100);
-        config.setMaxWaitMillis(10000);
-        if (addresses == null) {
-            pools = new JedisPool[]{new JedisPool(config, "localhost", 6379, DEFAULT_TIMEOUT, password)};
-            return;
-        }
-        ArrayList<JedisPool> pools_ = new ArrayList<>();
-        ArrayList<String> addrs_ = new ArrayList<>();
-        for (String address : addresses.split("[,;]")) {
-            String[] ss = address.split("[:/]");
-            if (ss.length >= 2) {
-                int port = Integer.parseInt(ss[1]);
-                if (port == 0) {
-                    continue;
-                }
-                addrs_.add(ss[0] + ':' + ss[1]);
-                pools_.add(new JedisPool(config, ss[0], port, DEFAULT_TIMEOUT, password));
-            }
-        }
-        if (pools_.isEmpty()) {
-            pools = new JedisPool[]{new JedisPool(config, "localhost", 6379, DEFAULT_TIMEOUT, password)};
-            addrs = new String[]{"localhost"};
-        } else {
-            pools = pools_.toArray(new JedisPool[0]);
-            addrs = addrs_.toArray(new String[0]);
-        }
-
-        String hello = pools[0].getResource().get("hello");
-        System.out.println("从 redis 获取值:" + hello);
+        return new JedisSentinelPool(masterName, sentinels, jedisPoolConfig, password);
     }
 
 }
