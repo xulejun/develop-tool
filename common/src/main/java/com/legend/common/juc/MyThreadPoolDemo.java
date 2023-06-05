e744b581989dea098654d9b64bdaf6d28f1bdaf0
lejunxu <lejunxu@trip.com>
2023-06-05 16:45:31 +0800

    feat：拒绝策略，工厂创建

--- common/src/main/java/com/legend/common/juc/MyThreadPoolDemo.java
+++ common/src/main/java/com/legend/common/juc/MyThreadPoolDemo.java
@@ -1,10 +1,13 @@
 package com.legend.common.juc;
 
+import cn.hutool.core.thread.ThreadFactoryBuilder;
+
 import java.util.concurrent.*;
 
 /**
  * 线程池样例
  * ThreadPoolExecutor的7大参数、4大拒绝策略
+ * hippo4j 可实现对线程池资源的监控
  *
  * @author XLJ
  * @date 2020/10/24
@@ -24,8 +27,12 @@ public class MyThreadPoolDemo {
                 TimeUnit.SECONDS,
                 new LinkedBlockingDeque<>(3),
                 Executors.defaultThreadFactory(),
-                // 默认拒绝策略AbortPolicy
-                new ThreadPoolExecutor.DiscardOldestPolicy());
+                // ThreadFactoryBuilder.create().build(),    // hutool、Appache-common、guava 都可创建
+
+                // new ThreadPoolExecutor.AbortPolicy();   // 默认拒绝策略——丢弃任务并抛出 RejectedExecutionException 异常
+                // new ThreadPoolExecutor.CallerRunsPolicy()  //  由调用线程执行任务
+                // new ThreadPoolExecutor.DiscardPolicy()     //  丢弃任务，但是不抛出异常
+                new ThreadPoolExecutor.DiscardOldestPolicy());      // 丢弃队列最早的未处理任务，然后重新尝试执行任务
         for (int i = 1; i <= 9; i++) {
             threadPoolExecutor.execute(() -> {
                 System.out.println(Thread.currentThread().getName() + "\t办理业务");
