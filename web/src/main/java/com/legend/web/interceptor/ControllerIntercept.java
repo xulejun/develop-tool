f09eb9fd2605417d8e5343af51ee88d3648d887e
602 <xuxing12@meituan.com>
2023-12-22 14:48:50 +0800

    feat：aop 拦截器日志输出

--- web/src/main/java/com/legend/web/interceptor/ControllerIntercept.java
+++ web/src/main/java/com/legend/web/interceptor/ControllerIntercept.java
@@ -1,8 +1,12 @@
 package com.xlj.tools.interceptor;
 
+import com.alibaba.fastjson2.JSON;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,9 +14,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Base64;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * 通过aop 实现接口调用校验
@@ -22,13 +31,14 @@ import java.util.Base64;
  */
 //@Component
 //@Aspect
+@Slf4j
 public class ControllerIntercept {
     private static final Logger LOGGER = LoggerFactory.getLogger(ControllerIntercept.class);
 
     @Autowired
     private HttpServletRequest request;
 
-    //    @Pointcut("execution(* com.xlj.tools.controller.*.*(..)) ")
+    //    @Pointcut("execution(public * com.legend.web..controller..*.*(..))")
     public void pointcut() {
     }
 
@@ -56,6 +66,43 @@ public class ControllerIntercept {
             LOGGER.error("process task error:{}, username:{}, apiType:{}", code, userName, apiType, e);
             // 参数null 可用通用response对象封装
             return ResponseEntity.status(HttpStatus.OK).body(null);
+        }
+    }
+
+    //    @Pointcut("execution(public * com.legend.web..controller..*.*(..))")
+    public void webLog() {
+    }
+
+    //    @Before("webLog()")
+    public void doBefore(JoinPoint joinPoint) {
+        String url = "";
+        String param = "";
+        String body = "";
+        try {
+            // 获取 HttpServletRequest
+            ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+            HttpServletRequest request = Objects.requireNonNull(attributes).getRequest();
+            // 用户
+            String userName = String.valueOf(request.getAttribute("userName"));
+            // 请求地址
+            url = request.getRequestURL().toString();
+            // 请求参数
+            Map<String, String[]> paramMap = request.getParameterMap();
+            if (!paramMap.isEmpty()) {
+                param = JSON.toJSONString(paramMap);
+            }
+            // 请求体
+            Object[] args = joinPoint.getArgs();
+
+            for (Object arg : args) {
+                if (arg instanceof HttpServletRequest || arg instanceof HttpServletResponse) {
+                    continue;
+                }
+                body = JSON.toJSONString(arg);
+            }
+            log.info("用户：{}，请求地址：{}，请求参数：{}，请求体：{}", userName, url, param, body);
+        } catch (Exception e) {
+            log.warn("AOP接口调用日志输出异常：url={}，param={}，body={}", url, param, body, e);
         }
     }
 
